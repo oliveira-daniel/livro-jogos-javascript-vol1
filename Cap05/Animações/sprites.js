d3d139bb804376633a1dd966fa225a792f53f3e7
Daniel de Oliveira <lacrimaopera@gmail.com>
2019-05-21 22:55:45 -0300

    Animação da explosão

--- Cap05/Animações/sprites.js
+++ Cap05/Animações/sprites.js
@@ -14,15 +14,19 @@ const context = canvas.getContext("2d");
 
 // Criar um objeto de imagem
 var img = new Image(),
-    index = 0;
-    sx = 192,
-    sy = 0,
-    sw = 192,
-    sh = 193,
-    dx = 0,
-    dy = 0,
-    dw = 192,
-    dh = 193;
+  frameIndex = 0,
+  frames = 13,
+  tickCount = 0
+  ticksFrame = 3,
+  loop = false;
+  sx = 192,
+  sy = 0,
+  sw = 192,
+  sh = 193,
+  dx = canvas.width  / 2 - 192/2,
+  dy = canvas.height / 2 - 192/2,
+  dw = 192,
+  dh = 193;
 
 // Definir a origem dessa nova imagem
 img.src = "assets/explosion_03_strip13.png";
@@ -34,16 +38,29 @@ img.onload = () => {
 };
 
 function animar() {
-  // Apaga o que está dentro do canvas
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  context.drawImage(img, sx * index, sy, sw, sh, dx, dy, dw, dh);
-
-  // Calcular os frames por segundo da explosão
-
-  index = (index + 3) % 3 == 0 < 13 ? ++index : 0;
 
   requestAnimationFrame(animar);
+
+  // --- Update
+  tickCount += 1;
+  if (tickCount > ticksFrame) {
+
+    tickCount = 0;
+
+    // Calcular os frames por segundo da explosão
+    if (frameIndex < frames - 1) {
+      // Go to the next frame
+      frameIndex += 1;
+    } else if (loop) {
+      frameIndex = 0;
+    }
+
+  }
+
+  // --- Render
+  // Apaga o que está dentro do canvas
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(img, frameIndex * sx, sy, sw, sh, dx, dy, dw, dh);
 }
 
 // Cada célula do Sprite Sheet
