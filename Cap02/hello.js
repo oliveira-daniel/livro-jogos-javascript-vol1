e558111bfd13b8839a6121e5edf355556b29565d
Daniel de Oliveira <lacrimaopera@gmail.com>
2019-05-24 01:18:51 -0300

    Ajuste de codicifação de texto

--- Cap02/hello.js
+++ Cap02/hello.js
@@ -2,5 +2,6 @@
 // Disparando uma mensagem de olá mundo no navegador
 alert("Olá Mundo no JavaScript!");
 
+// Depois de clicar em "ok"...
 // Disparando uma mensagem de olá mundo agora no console
 console.log("Olá Console!");
