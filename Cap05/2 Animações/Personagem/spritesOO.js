e0a52c92ccab19c6201cbd44d96aa9b13a816400
Daniel-Oliveira <lacrimaopera@gmail.com>
2019-06-04 21:18:30 -0300

    Experimentos Sprites e Objetos

--- Cap05/2 Animações/Personagem/spritesOO.js
+++ Cap05/2 Animações/Personagem/spritesOO.js
@@ -113,9 +113,8 @@ _defs.img = new Image();
 _defs.img.src = _defs.imgSrc;
 
 _defs.img.onload = () => {
-  console.log("nova imagem");
-  context.imageSmoothingEnabled = false;
-  animar();
+  _defs.ctx2d.imageSmoothingEnabled = false;
+  // animar();
 }
 
 function animar() {
@@ -147,3 +146,119 @@ function animar() {
 }
 
 // new Sprite(_defs);
+class GameObject {
+  constructor () {
+    Game.addGameObj(this);
+  }
+}
+
+class Sprite2d extends GameObject {
+
+  constructor (ctx2d, img) {
+    super();
+    this.ctx = ctx2d;
+    this.img = img;
+
+    this._defs = {
+      frameIndex: 0,
+      frames: 13,
+      ticksFrame: 4,
+      sx: 43,
+      sy: 0,
+      sw: 43,
+      sh: 37,
+      dx: canvas.width / 2 - 43 / 2 * 4,
+      dy: canvas.height / 2 - 43 / 2 * 4,
+      dw: 43 * 4,
+      dh: 37 * 4,
+      loop: true,
+      tickCount: 0,
+    }
+  }
+
+  draw () {
+    // this.ctx.fillStyle = "#FF0000";
+    // this.ctx.drawImage(this.img, 0, 0);
+
+    // --- Update
+    this._defs.tickCount += 1;
+
+    if (this._defs.tickCount > this._defs.ticksFrame) {
+
+      this._defs.tickCount = 0;
+
+      // Calcular os frames por segundo da explosão
+      if (this._defs.frameIndex < this._defs.frames - 1) {
+        // Go to the next frame
+        this._defs.frameIndex += 1;
+      } else if (_defs.loop) {
+        this._defs.frameIndex = 0;
+      }
+
+    }
+
+    _defs.ctx2d.drawImage(this.img, this._defs.frameIndex * this._defs.sx, this._defs.sy,
+      this._defs.sw, this._defs.sh, this._defs.dx, this._defs.dy, this._defs.dw, this._defs.dh);
+  }
+
+}
+
+class Game {
+
+  constructor (args) {
+    Game.gObjcts = new Array();
+    this.canvas = args.canvas;
+    this.ctx = this.canvas.getContext("2d");
+  }
+
+  //
+  static addGameObj (gameObject) {
+    Game.gObjcts.push(gameObject);
+  }
+
+  static get gameObjects () {
+    return this.gObjcts;
+  }
+
+  render () {
+    Game.gameObjects.forEach(go => {
+      go.draw();
+    });
+  }
+
+  start () {
+
+    this.update();
+  }
+
+  update () {
+    requestAnimationFrame(() => this.update());
+
+    // console.log("animando...");
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.render();
+  }
+
+}
+
+window.onload = () => {
+  let game = new Game({
+    canvas: canvas
+  });
+
+  let imgCaveira = new Image();
+  imgCaveira.src = "../assets/Skeleton Attack.png";
+
+  imgCaveira.onload = () => {
+    let caveira = new Sprite2d(context, imgCaveira);
+
+    game.start();
+  }
+
+
+
+  //console.log(Game.gameObjects);
+
+  // game.start();
+}
