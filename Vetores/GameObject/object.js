cd4f556b9ecbe3ebe324158b71a35fc6888181fd
Daniel de Oliveira <lacrimaopera@gmail.com>
2019-06-16 02:14:29 -0300

    Trabalhando com GameObjects

--- Vetores/GameObject/object.js
+++ Vetores/GameObject/object.js
@@ -69,6 +69,8 @@ class GameObject {
   constructor () {
     Game.addGameObj(this);
   }
+
+  update () {}
 }
 
 class PlayerObject extends GameObject {
@@ -79,10 +81,15 @@ class PlayerObject extends GameObject {
     this.vel = new Vector(0, 0);
     this.acc = new Vector(0, 0);
     this.ctx2d = Game.ctx;
+    this.moving = false;
   }
 
   get velMov () {
-    return 4;
+    return 7;
+  }
+
+  set colisionMap (map) {
+    this.map = map;
   }
 
   draw () {
@@ -98,30 +105,43 @@ class PlayerObject extends GameObject {
 
   update () {
 
-    if (Game.key == "ArrowLeft") {
-      // this.applyForce(new Vector(-.5, 0));
-      this.vel.set(-this.velMov, 0);
-    }
+    if (!this.moving) {
+      if (Game.key == "ArrowLeft") {
+        // this.applyForce(new Vector(-.5, 0));
+        this.vel.set(-this.velMov, 0);
+      }
 
-    if (Game.key == "ArrowRight") {
-      // this.applyForce(new Vector(.5, 0));
-      this.vel.set(this.velMov, 0);
-    }
+      if (Game.key == "ArrowRight") {
+        // this.applyForce(new Vector(.5, 0));
+        this.vel.set(this.velMov, 0);
+      }
 
-    if (Game.key == "ArrowUp") {
-      // this.applyForce(new Vector(0, -.5));
-      this.vel.set(0, -this.velMov);
-    }
+      if (Game.key == "ArrowUp") {
+        // this.applyForce(new Vector(0, -.5));
+        this.vel.set(0, -this.velMov);
+      }
 
-    if (Game.key == "ArrowDown") {
-      // this.applyForce(new Vector(0, .5));
-      this.vel.set(0, this.velMov);
+      if (Game.key == "ArrowDown") {
+        // this.applyForce(new Vector(0, .5));
+        this.vel.set(0, this.velMov);
+      }
     }
 
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
 
+    this.verifyEdges();
+
+    if (this.vel.x > 0 || this.vel.x < 0 ||
+        this.vel.y > 0 || this.vel.y < 0) {
+      this.moving = true;
+    } else {
+      this.moving = false;
+    }
+  }
+
+  verifyEdges () {
     if (this.pos.x < 0) {
       this.vel.x *= 0;
       this.pos.x = 0;
@@ -142,93 +162,82 @@ class PlayerObject extends GameObject {
       this.pos.y = Game.limits.maxy - 20;
     }
 
+    // console.log( Math.floor(this.pos.x / 20) , Math.floor(this.pos.y / 20) );
+    var mapX = Math.floor((this.pos.x) / 20);
+    var mapY = Math.floor((this.pos.y) / 20);
+
+    // Se deslocando para a direita
+    if (this.vel.x > 0 && this.map[(mapX + mapY * 12) + 1] == 1) {
+      this.vel.x *= 0;
+      this.pos.x = 20 * mapX;
+    }
+
+    // Se deslocando para a esquerda
+    if (this.vel.x < 0 && this.map[(mapX + mapY * 12)] == 1) {
+      this.vel.x *= 0;
+      this.pos.x = 20 * mapX + 20;
+    }
+
+    // Se deslocando para baixo
+    if (this.vel.y > 0 && this.map[(mapX + mapY * 12) + 12] == 1) {
+      this.vel.y *= 0;
+      this.pos.y = 20 * mapY;
+    }
+
+    // Se deslocando para baixo
+    if (this.vel.y < 0 && this.map[(mapX + mapY * 12)] == 1) {
+      this.vel.y *= 0;
+      this.pos.y = 20 * mapY + 20;
+    }
   }
 
 }
 
-class Sprite2d extends GameObject {
+class TileMap extends GameObject {
 
-  constructor (ctx2d, estados) {
+  constructor (x, y, color) {
     super();
-    this.ctx = ctx2d;
-    this.canvas = ctx2d.canvas;
-    this.frameIndex = 0;
-    this.tickCount = 0;
-
-    for (let key in estados) {
-      estados[key].img.src = estados[key].imgSRC;
-    }
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.ctx2d = Game.ctx;
   }
 
   draw () {
-    // --- Update
-    this.update();
-
-    this.tickCount += 1;
-    if (this.tickCount > this.estadoAtual.ticksFrame) {
-      this.tickCount = 0;
-      // Calcular os frames por segundo
-      if (this.frameIndex < this.estadoAtual.frames - 1) {
-        // Avançar ao próximo frame
-        this.frameIndex += 1;
-      } else if (this.estadoAtual.loop) {
-        // Verifica se deve executar a animação em loop
-        this.frameIndex = 0;
-      }
-    }
-
-    this.ctx.drawImage(this.estadoAtual.img, this.frameIndex * this.estadoAtual.sw,
-      0, this.estadoAtual.sw, this.estadoAtual.sh, this.estadoAtual.dx,
-      this.estadoAtual.dy, this.estadoAtual.dw, this.estadoAtual.dh);
+    // Definir uma cor ao desenho
+    this.ctx2d.fillStyle = this.color;
+    // Desenhar um retângulo
+    this.ctx2d.fillRect(this.x, this.y, 20, 20);
   }
-
-}
-
-class BackgroundParallax extends GameObject {
-
-  constructor (ctx2d) {
-    super();
-    this.layers = new Array();
-    this.ctx = ctx2d;
-  }
-
-  addLayer(newLayer, delta, w, h, x, y) {
-    let img = new Image();
-    img.src = newLayer;
-    this.layers.push({img: img, x: x, y: y, w: w, h: h, delta: delta});
-  }
-
-  draw () {
-    this.layers.forEach(layer => {
-      /*if (layer.x >= -layer.w) {
-        layer.x -= layer.delta;
-      } else {
-        layer.x += layer.w-layer.delta;
-      }*/
-
-
-
-      this.ctx.drawImage(layer.img, layer.x + layer.w-1, layer.y, layer.w, layer.h);
-      this.ctx.drawImage(layer.img, layer.x, layer.y, layer.w, layer.h);
-    });
-  }
-
 }
 
 class Vector {
-  constructor(x, y) {
+  constructor (x, y) {
     this.x = x;
     this.y = y;
   }
 
-  add(vector) {
+  add (vector) {
     this.x += vector.x;
     this.y += vector.y;
   }
 
-  set(x, y) {
+  set (x, y) {
     this.x = x;
     this.y = y;
+  }
+}
+
+function createMap (map) {
+  let posx = 0;
+  let posy = 0;
+  for (let i = 0; i < map.length; i++) {
+    if (map[i] == 1) new TileMap (posx, posy, "#6699cc");
+    else new TileMap (posx, posy, "#3333cc");
+    if (posx % 220 == 0 && posx > 0) {
+      posx = 0;
+      posy += 20;
+    } else posx += 20;
   }
 }
 
@@ -237,78 +246,30 @@ window.onload = () => {
   // Selecionando o elemento canvas
   let canvas = document.querySelector("#game-canvas");
 
-  canvas.focus();
-
   // Obter o contexto 2d
   let context = canvas.getContext("2d");
   context.imageSmoothingEnabled = false;
 
-  let game = new Game({
+  let game = new Game ({
     canvasId: "game-canvas"
   });
 
-  let player1 = new PlayerObject(280, 280);
+  let gameMap = [
+    0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1,
+    0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1,
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  ];
+
+  createMap(gameMap);
+
+  let player1 = new PlayerObject (0, 0);
+  player1.colisionMap = gameMap;
 
   game.start();
-
-  let player = new Vector(20, canvas.height);
-  let vel    = new Vector(2.5, 0);
-  let jump   = new Vector(0, -10);
-  let gravity = new Vector(0, 0.5);
-  let jumping = false;
-
-  let obstaculo = new Vector(420, canvas.height);
-
-  function update() {
-      requestAnimationFrame(update);
-      clear();
-
-      // Definir uma cor ao desenho
-      context.fillStyle = "#9999cc";
-      // Desenhar um retângulo
-      context.fillRect(player.x, player.y - 60, 20, 60);
-
-      // Definir uma cor ao desenho
-      context.fillStyle = "#999966";
-      context.fillRect(obstaculo.x, obstaculo.y - 40, 20, 40);
-
-      if (jumping) {
-        vel.add(jump);
-        jump.set(0, 0);
-      }
-
-      context.translate(-vel.x, 0);
-
-      vel.add(gravity);
-      player.add(vel);
-
-      if (player.y > canvas.height) {
-        vel.y *= 0;
-        player.y = canvas.height;
-        jump.set(0, -10);
-        jumping = false;
-      }
-  }
-
-  function clear() {
-    context.save();
-    context.setTransform(1, 0, 0, 1, 0, 0);
-    // Will always clear the right space
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.restore();
-  }
-
-  function keyPressed(e) {
-    // Armazenar a tecla pressionada.
-    var k = e.key;
-
-    if (k == ' ' && !jumping) {
-      jumping = true;
-    }
-  }
-
-  // addEventListener("keydown", keyPressed);
-
-  // update();
 
 }
